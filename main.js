c3a10a5738d59810ebc56c97ffbe50024e4f4d76
safiyahniaz <safiyahniaz@yahoo.com>
2019-05-25 15:38:37 +0800

    added "by days"
    added "by days"and all its codes

--- main.js
+++ main.js
@@ -23,3 +23,29 @@ function calculateDaysNeeded(){
     // output the result on the card
     resultArea.innerHTML = message
 }
+
+function calculatePagesNeeded(){
+    // get the value from the input field
+
+    const dayInputElement = document.getElementById('first_name3')
+    const dayInputValue = dayInputElement.value
+    console.log(dayInputValue)
+    //  const pageInputValue = document.getElementById('first_name2').value //
+
+
+    // use formula to find page needed
+    let total = 604
+    let result = total / dayInputValue
+    const niceResult = result.toFixed(2)
+    console.log('result', result)
+
+    // get the element of the card
+    const resultArea = document.getElementById('result1')
+    let message = 'Please input a valid number of pages'
+
+    if (dayInputValue && dayInputValue > 0) {
+        message = 'If you want to khatam the Quran in  ' + dayInputValue + ' day(s), you will have to read about ' + niceResult + ' pages daily.'
+    }
+    // output the result on the card
+    resultArea.innerHTML = message
+}
